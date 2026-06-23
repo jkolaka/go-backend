ada1f5e143ccd09b7ecaad0b50fe8e95bb35c35f
jkolaka <joebeckhamkolaka@gmail.com>
2026-06-24 01:01:00 +0300

    search recipe by tag

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
 )
@@ -56,7 +56,7 @@ func UpdateRecipeHandler(c *gin.Context) {
 	for i := 0; i < len(recipes); i++ {
 		if recipes[i].ID == id {
 			index = i
-			break 
+			break
 		}
 	}
 	if index == -1 {
@@ -79,7 +79,7 @@ func DeleteRecipeHandler(c *gin.Context) {
 	for i := 0; i < len(recipes); i++ {
 		if recipes[i].ID == id {
 			index = i
-			break 
+			break
 		}
 	}
 	if index == -1 {
@@ -90,15 +90,34 @@ func DeleteRecipeHandler(c *gin.Context) {
 	}
 	recipes = append(recipes[:index], recipes[index+1:]...)
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Recipe has been deleted", 
+		"message": "Recipe has been deleted",
 	})
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes,
+				recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
-	router.DELETE("/recipes/:id", DeleteRecipeHandler) 
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
